36710fbcdacbe66c8e69a0118f784d353a3c8f7f
Chris Coles <cryan.coles@gmail.com>
2018-09-11 19:25:12 -0400

    Updates.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import csv
 import os
 from typing import Sequence
 
@@ -12,12 +13,12 @@ from csvparser.lib.parser import get_csv_reader, get_csv_writer
 exceptions_manager = ExceptionsManager.getInstance()
 
 
-def normalize_csv_header(is_header: bool, row: Sequence[str], new_csv) -> None:
+def normalize_csv_header(is_header: bool, row: Sequence[str], csv_writer: csv.writer) -> None:
     if is_header:
-        new_csv.writerow(row)
+        csv_writer.writerow(row)
 
 
-def normalize_csv_body(is_body: bool, row: Sequence[str], new_csv) -> None:
+def normalize_csv_body(is_body: bool, row: Sequence[str], csv_writer: csv.writer) -> None:
     if is_body:
         timestamp, address, zipcode, full_name, foo, bar, total, notes = \
             row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7]
@@ -25,15 +26,15 @@ def normalize_csv_body(is_body: bool, row: Sequence[str], new_csv) -> None:
         foo_duration = normalize_duration(foo, 'foo')
         bar_duration = normalize_duration(bar, 'bar')
 
-        new_csv.writerow([
+        csv_writer.writerow([
             normalize_timestamp(timestamp), normalize_address(address), normalize_zipcode(zipcode), normalize_name(full_name),
             foo_duration, bar_duration, normalize_total_duration(foo_duration, bar_duration), normalize_notes(notes) ])
 
 
-def normalize_csv(read_file: str, write_file) -> None:
+def normalize_csv(read_file: str, csv_writer: csv.writer) -> None:
     for idx, row in enumerate(read_file):
-        normalize_csv_header(idx == 0, row, write_file)
-        normalize_csv_body(idx != 0 and len(row) != 0, row, write_file)
+        normalize_csv_header(idx == 0, row, csv_writer)
+        normalize_csv_body(idx != 0 and len(row) != 0, row, csv_writer)
 
 
 def normalize() -> None:
@@ -50,8 +51,8 @@ def normalize() -> None:
                 with open(file_to_transform, encoding='utf8', errors='replace') as csvfile:
                     return normalize_csv(get_csv_reader(csvfile), get_csv_writer(new_csvfile))
         raise FileNotFoundError
-    else:
-        raise TransformFileMustExistError
+
+    raise TransformFileMustExistError
 
 
 
